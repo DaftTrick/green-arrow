b19c43c938d61e0d44f7d94379fea35013ec2035
Oleg Nazarov <onazarov@os33.com>
2018-07-10 17:30:33 +0300

    error 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '/404', :to => 'errors#not_found', :via => :all, as: :not_found
+
   resources :high_scores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
